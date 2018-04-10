bd9273e6d4fd535bd9260601df421fe68d48e7d1
zicsus <zicsus@gmail.com>
2018-04-11 00:37:02 +0530

    get top 5 news

--- Feedy.py
+++ Feedy.py
@@ -3,12 +3,13 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
 import time
-from Parser.Parser import *
+from Parser.Parser import Parser
 
 Client = discord.Client()
-client = commands.Bot(command_prefix = "?")
+client = commands.Bot(command_prefix = "!")
 
-parser = Parser()
+url = ""
+parser = Parser(url)
 
 @client.event
 async def on_ready():
@@ -16,12 +17,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if(message.content == "cookie"):
-        await client.send_message(message.channel, ":cookie:")
+    if(message.content.lower() == "!help"):
+        response = "I don't do much at the time. but here's what i got \n -> !top5 : Get latest latest 5 gaming news"
+        await client.send_message(message.channel, response)
 
-    if(message.content.startswith == "!F"):
-        parser.getLatestFeed()
-        await client.send_message(message.channel, "Getting latest feed")
+    elif(message.content == "!top5"):
+        await client.send_message(message.channel, "Getting top 5 news. Please Wait...")
+        parser.update()
+        top5 = parser.top5()  
+        for x in top5:
+            response = x["title"] + "\n\n"
+            response += x["description"][0:200] + "... \n"
+            response += x["media"]
+            await client.send_message(message.channel, response)
+    
+    elif()
+        
 
-
-client.run("YOUR_TOKEN")
+client.run("NDIyNDUzNjA2NjU0OTM1MDQz.DYcBsQ.ftZTzQxbKEU1Kif5m5PRyjLYH00")
--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,9 +1,37 @@
 import feedparser
+import re
 
 class Parser:
     
-    def __init__(self):
-        print("getting feed")
+    platforms = {
+        "ign": {
+            "all" : "http://feeds.ign.com/ign/games-all"
+        },
+        "gamespot": {
+            "all" : "https://www.gamespot.com/feeds/mashup/"
+        }
+    }
 
-    def getLatestFeed(self):
-        print("latest feed")
+    def __init__(self, url):
+        self.url = url
+
+    def update(self):
+        self.__feeds = feedparser.parse(self.url).entries
+
+    def top5(self):
+        top5 = []
+        for i in range(0, 5):
+            feed = self.__feeds[i]
+            title = feed.title
+            description = self.cleanhtml(feed.description)
+            media = feed.media_content[0]['url']
+            top5.append({"title":title, "description":description, "media":media})
+
+        return top5
+
+    def cleanhtml(self, raw_html):
+        cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', raw_html)
+        return cleantext
+
+
